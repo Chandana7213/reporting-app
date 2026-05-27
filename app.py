8773c1f3deb25d58d4a53f29b6fe67c045358970
Chandana7213 <dasarichandana12@gmail.com>
2026-05-27 21:27:41 +0530

    Layer 7: Test CI/CD trigger

--- app.py
+++ app.py
@@ -173,3 +173,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     # host=0.0.0.0 is REQUIRED so the container is reachable from outside.
     app.run(host="0.0.0.0", port=port, debug=True)
+# Testing CI/CD pipeline - Layer 7
